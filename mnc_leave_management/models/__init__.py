07c6bcfd80db880396bd825772b372af79200bf8
MNC-CORPORATION\mochammad.naufal <mochammadrizanaufal@gmail.com>
2021-12-29 17:58:36 +0700

    [UPDATE] Time Off Type field on Leave Report

--- mnc_leave_management/models/__init__.py
+++ mnc_leave_management/models/__init__.py
@@ -4,4 +4,5 @@ from . import hr_timeoff
 from . import hr_timeoff_allocation
 from . import hr_timeoff_type
 from . import hr_timeoff_policy
+from . import hr_leave_report
 # from . import res_users
